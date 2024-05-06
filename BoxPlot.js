dcddf9bb76c415d4c4f97e68141a6462888c84ac
Path3m <150528556+Path3m@users.noreply.github.com>
2024-05-06 17:25:01 +0200

    add : class to make easier the creation of box plot

--- BoxPlot.js
+++ BoxPlot.js
@@ -1,67 +1,86 @@
 import * as util from "./optigen/utilgen.js";
+
 import { Permutation } from "./optigen/Permutation.js";
-import { Individual } from "./optigen/Individuals.js";
-import { Population } from "./optigen/Population.js";
-import { optigen, dimensions, influences } from "./optigen/algogen.js";
 
-function maxOfMat(matrix){
-    return matrix.reduce((acc, current) => { return Math.max(acc, Math.max(...current)); }, 0);
+export class OptigenBoxPlot{
+    static DEFAULT_COLORS = [
+        "rgb(0,0,0)",
+        "rgb(255,0,0)",
+        "rgb(0,255,0)",
+        "rgb(0,0,255)",
+    ];
+
+    /**
+     * Build the data of the box plot and set it ready to be drown
+     * @param {*} div 
+     * @param {*} geneticResult 
+     */
+    constructor(div, geneticResult){
+        let tmp = this.buildData(geneticResult.statistic);
+        let data = tmp.data;
+        let range = tmp.range;
+
+        this.div = div;
+        this.data = data;
+        this.layout = this.buildLayout(range);
+    }
+
+    /**
+     * Build the data
+     * @param {*} statGenerations 
+     * @returns 
+     */
+    buildData(generationDatas){
+        var colors = OptigenBoxPlot.DEFAULT_COLORS;
+        console.log(generationDatas);
+
+        var xData = Permutation.index(generationDatas.length);
+        var data = [], max = 0, min = 0;
+
+        for ( var i = 0; i < xData.length; i ++ ) {
+            max = Math.max(max, Math.max(...generationDatas[i]));
+            min = Math.min(max, Math.min(...generationDatas[i]));
+            var result = { type: 'box', boxmean: true, boxpoints: false,
+                line:{color:colors[i%colors.length]},
+                y: generationDatas[i],
+                name: "Gen "+xData[i]};
+            data.push(result);
+        };
+
+        return {data:data, range:{max:max,min:min}};
+    }
+
+    /**
+     * Build the layout
+     * @param {*} yRange 
+     * @returns 
+     */
+    buildLayout(yRange){
+        var layout = {
+            title: 'Genetic Optimisation',
+            yaxis: {
+                range: yRange,
+                showgrid: true,
+                zeroline: true,
+                dtick: Math.ceil((yRange.max-yRange.min)/20),
+                gridcolor: 'rgb(255, 255, 255)',
+                gridwidth: 1,
+                zerolinecolor: 'rgb(255, 255, 255)',
+                zerolinewidth: 2
+            },
+            margin: { l: 40, r: 30, b: 80, t: 100 },
+            paper_bgcolor: 'rgb(243, 243, 243)',
+            plot_bgcolor: 'rgb(243, 243, 243)',
+            showlegend: false
+        };
+
+        return layout;
+    }
+    
+    /**
+     * Draw the box plot
+     */
+    draw(){
+        Plotly.newPlot('myDiv', this.data, this.layout);
+    }
 }
-
-function minOfMat(matrix){
-    return matrix.reduce((acc, current) => { return Math.min(acc, Math.min(...current)); }, 0);
-}
-
-var geneticResult = optigen(util.score, dimensions, influences);
-var finalpop = geneticResult.bestPopulation;
-
-var yData = geneticResult.statistic;
-console.log(yData);
-var yRange = {min:minOfMat(yData), max:maxOfMat(yData)};
-
-var xData = Permutation.index(yData.length);
-var colors = [
-    "rgb(0,0,0)",
-    "rgb(255,0,0)",
-    "rgb(0,255,0)",
-    "rgb(0,0,255)",
-];
-
-var data = [];
-
-for ( var i = 0; i < xData.length; i ++ ) {
-    var result = {
-        type: 'box',
-        y: yData[i],
-        name: "Gen "+xData[i],
-        boxmean: true,
-        boxpoints: false,
-        line:{color:colors[i%colors.length]}
-    };
-    data.push(result);
-};
-
-var layout = {
-    title: 'Genetic Optimisation',
-    yaxis: {
-        range: yRange,
-        showgrid: true,
-        zeroline: true,
-        dtick: Math.ceil((yRange.max-yRange.min)/20),
-        gridcolor: 'rgb(255, 255, 255)',
-        gridwidth: 1,
-        zerolinecolor: 'rgb(255, 255, 255)',
-        zerolinewidth: 2
-    },
-    margin: {
-        l: 40,
-        r: 30,
-        b: 80,
-        t: 100
-    },
-    paper_bgcolor: 'rgb(243, 243, 243)',
-    plot_bgcolor: 'rgb(243, 243, 243)',
-    showlegend: false
-};
-
-Plotly.newPlot('myDiv', data, layout);
