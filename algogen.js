faa764c8f65d8d8da04f3395515a121789feea70
Path3m <150528556+Path3m@users.noreply.github.com>
2024-05-06 11:00:30 +0200

    refactoring : main and algogen mofified according to the new shiftLeft operator for mutation, and test exec

--- algogen.js
+++ algogen.js
@@ -4,53 +4,50 @@ import { Permutation } from "./Permutation.js";
 import { Individual } from "./Individuals.js";
 import { Population } from "./Population.js";
 
-export function optigen(score){
-    let nbGen   = 200;
-    let nbInd   = 50;
-    let sizeInd = 10;
+export function optigen(score, dimensions, influenceFactors){
+    let limit   = dimensions.limit;
+    let sizeGen = dimensions.generation;
+    let sizeInd = dimensions.individual;
 
-    let pr     = 0.6;
-    let pm     = 0.5;
-    let pselec = 0.3;
+    let reproduction = influenceFactors.reproduction;
+    let mutation     = influenceFactors.mutation;
+    let selection    = influenceFactors.selection;
 
     let index = new Individual( Permutation.index(sizeInd), score );
-    let population = Population.randPop(nbInd, sizeInd, score);
+    let population = Population.randPop(sizeGen, sizeInd, score);
 
     console.log("Initialisation :");
     population.poplog();
 
     population.sortByScore();
 
-    for(let i=0; i<nbGen; i++){
+    for(let i=0; i<limit; i++){
 
         console.log("Generation ",i," population triée :");
         population.poplog();
 
-        let nbMeilleurs = Math.ceil(nbInd * pselec);
+        let nbMeilleurs = Math.ceil(sizeGen * selection);
         console.log("n meilleurs : ",nbMeilleurs);
+        let meilleurs = population.selectMeilleurs(nbMeilleurs);
+        meilleurs.poplog();
 
-        let meilleursInd = new Array(nbMeilleurs);
-        for(let j=0; j<meilleursInd.length; meilleursInd[j] = population.members[j++]);
-        let meilleurs = new Population(meilleursInd, population.func);
-
-        let nbChildren = nbInd - nbMeilleurs;
+        let nbChildren = sizeGen - nbMeilleurs;
         console.log("m enfants : ",nbChildren);
 
-        let newGen = population.newGeneration(pr, nbChildren);
+        let newGen = population.newGeneration(reproduction, nbChildren);
 
         console.log("Generation ",i," enfants :");
         newGen.poplog();
 
-        newGen.mutatePop(pm);
+        newGen.mutatePop(mutation);
         console.log("Generation ",i," enfants mutés :");
         newGen.poplog();
 
-        newGen.concat(meilleurs);
-        population = newGen.sortByScore();
+        population = (newGen.push(meilleurs)).sortByScore();
 
         if(population.members[0].score === index.score){
             console.log("\nMAXIMUM REACH AT GENERATION ",i,"\n");
-            i = nbGen + 1;
+            i = limit + 1;
         }
     }
 
